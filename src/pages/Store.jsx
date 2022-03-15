1c35a11019ee85188ad3287ee0e2dfea4ce73f0a
Mujahid-Developer <mujahidulislam575@gmail.com>
2022-03-15 19:51:44 +0600

    fix store page issue

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -7,7 +7,7 @@ const Store = () => {
   return (
     <>
       <Navbar />
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 min-h-screen">
         <TopCollections />
       </div>
       <Footer />
