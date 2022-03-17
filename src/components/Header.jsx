752a8bfbbcb717e44c0095255c272e4aeaa7dd37
Mujahid-Developer <mujahidulislam575@gmail.com>
2022-03-18 03:24:02 +0600

    fix update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,26 +21,29 @@ const Header = () => {
         <div className="relative min-h-96">
           <div>
             <div className="bg-gradient-to-r from-violet-900/80 via-violet-900/80 to-violet-900/80 backdrop-blur-xs absolute w-2/4 h-full flex flex-col justify-center items-end text-white rounded-r-xl">
-              <div className=" w-full md:px-8 px-3 flex flex-col justify-center gap-2 md:gap-6 h-full">
+              <div className=" w-full md:px-8 px-3 flex flex-col justify-center gap-1 md:gap-6 h-full">
                 <h1 className="text-thin md:text-lg text-xs">
                   New collection droped
                 </h1>
-                <h1 className="font-bold md:text-3xl text-lg">
+                <h1 className="font-bold md:text-3xl text-sm">
                   Savage Homosapiens
                 </h1>
                 <h1 className="text-xs md:text-lg">
                   Welcome to the home of Savage Gang on OpenSea. Discover the
                   best items in this collection.
                 </h1>
-                <h1 className="text-xs md:text-lg">
+                <h1 className="text-xs md:text-lg md:block hidden">
                   Time left of 10% Discount
                 </h1>
-                <div className="flex gap-2 md:gap-4 justify-between items-center">
-                  <h1 className="md:text-2xl" style={{ fontFamily: "Aldrich" }}>
+                <div className="flex gap-1 md:gap-4 justify-between items-center">
+                  <h1
+                    className="md:text-2xl text-sm"
+                    style={{ fontFamily: "Aldrich" }}
+                  >
                     20h 8m 52s
                   </h1>
-                  <button className="md:btn py-2 px-3 rounded-lg md:px-6 bg-gradient-to-r from-violet-500 to-violet-400 border-none md:text-white text-xs">
-                    View Drops
+                  <button className="md:btn md:btn-lg btn-sm py-2 px-4 rounded-lg md:px-6 bg-gradient-to-r from-violet-500 to-violet-400 border-none md:text-white text-xs">
+                    OpenSea
                   </button>
                 </div>
               </div>
